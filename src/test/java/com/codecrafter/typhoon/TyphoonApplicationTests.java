11dbf109d96ba3fe7aa4b60c8f8392a15a1af2ab
jayce8887 <158434815+jayce8887@users.noreply.github.com>
2024-02-08 18:33:31 +0900

    initail commit
    initail commit

---
+++ src/test/java/com/codecrafter/typhoon/TyphoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecrafter.typhoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TyphoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
