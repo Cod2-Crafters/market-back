fea01d951eea8d8a32992c62df413e6f48b23cac
jiheon2234 <jiheon2234@gmail.com>
2024-02-17 11:47:52 +0900

    MemberResolver구현 컨트롤러에서 Member member로 받기 가능 대신, 인증객체없으면 무조건 익셉션터짐으로, 인증이 필요한 로직에만 써야됨

---
+++ src/main/java/com/codecrafter/typhoon/config/resolver/MemberResolver.java
@@ -0,0 +1,41 @@
+package com.codecrafter.typhoon.config.resolver;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import com.codecrafter.typhoon.config.MockPrincipal;
+import com.codecrafter.typhoon.domain.entity.Member;
+import com.codecrafter.typhoon.exception.NoMemberException;
+import com.codecrafter.typhoon.repository.member.MemberRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class MemberResolver implements HandlerMethodArgumentResolver {
+
+	private final MemberRepository memberRepository;
+
+	@Override
+	public boolean supportsParameter(MethodParameter parameter) {
+		return parameter.getParameterType().equals(Member.class);
+	}
+
+	@Override
+	public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+		NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+		MockPrincipal principal = (MockPrincipal)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		Member member = memberRepository.findById(principal.getId()).orElseThrow(NoMemberException::new);
+		return member;
+	}
+
+	public MemberResolver(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
+
+}
